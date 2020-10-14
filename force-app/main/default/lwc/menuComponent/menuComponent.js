935b91524fa20c8efa95d081050241c73f072dc1
Vladzensan <vladzensan@gmail.com>
2020-10-15 00:30:57 +0300

    Improve logic and markup

--- force-app/main/default/lwc/menuComponent/menuComponent.js
+++ force-app/main/default/lwc/menuComponent/menuComponent.js
@@ -1,19 +1,34 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getDishes from '@salesforce/apex/MenuController.getDishes';
 
 export default class MenuComponent extends LightningElement {
-    @api dishes;
+    searchBarIsVisible = true;
+    filters = {};
+    pageNumber = 1;
+
+    pageSize;
+ 
+    totalItemCount = 0;
+
+    @wire(getDishes, { filters: '$filters', pageNumber: '$pageNumber' }) dishes;
+    selectedDish;
 
     handleSelected(event) {
-        try {
-        const evt = new CustomEvent('dishselected', {
-            // detail contains only primitives
-            detail: {dishId: event.detail.dishId}
-        });
-        // Fire the event from c-tile
-        this.dispatchEvent(evt);
-        console.log('menu');
-    }catch(e) {
-        console.log('menu' + e)
+            this.selectedDish = this.dishes.data.records.find((dish) => dish.Id === event.detail.dishId);
     }
+
+    handleSearchKeyChange(event) {
+        this.filters = {
+            searchKey: event.target.value.toLowerCase()
+        };
+        this.pageNumber = 1;
+    }
+
+    handlePreviousPage() {
+        this.pageNumber = this.pageNumber - 1;
+    }
+
+    handleNextPage() {
+        this.pageNumber = this.pageNumber + 1;
     }
 }
